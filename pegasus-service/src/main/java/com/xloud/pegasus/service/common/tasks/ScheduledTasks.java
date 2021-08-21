ca19477b76e072fc4e86712d441f6e9e6add53e0
samehada-momojiri <samehada.momojiri@gmail.com>
2021-08-21 14:39:15 +0900

    ・Tasksを追加

---
+++ pegasus-service/src/main/java/com/xloud/pegasus/service/common/tasks/ScheduledTasks.java
@@ -0,0 +1,18 @@
+package com.xloud.pegasus.service.common.utils;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTasks.class);
+
+	@Scheduled(initialDelay = 60000, fixedRate = 10000)
+	public void doSomething() {
+		LOGGER.info("### This time is : ", DateUtils.format(new Date()));
+	}
+}
