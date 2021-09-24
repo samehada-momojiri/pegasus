03846b290d6fa035ab06bf57fc9742145f7ed9df
samehada-momojiri <samehada.momojiri@gmail.com>
2021-09-24 13:01:17 +0900

    SampleControllerを作成

--- pegasus-common/src/main/java/com/xloud/pegasus/constants/CommonConstants.java
+++ pegasus-common/src/main/java/com/xloud/pegasus/constants/CommonConstants.java
@@ -5,8 +5,19 @@ public class CommonConstants {
 	private CommonConstants() {
 	}
 
+	/** 処理結果フラグ：成功. */
 	public static final String API_RESPONSE_SUCCESS = "0";
 
+	/** 処理結果フラグ：失敗. */
 	public static final String API_RESPONSE_ERROR = "1";
+
+	/** URLのベース部分：API用. */
+	public static final String URL_BASE_API = "/${spring.profiles.active}/${application.version:v0.1}/api";
+
+	/** URLのベース部分：SPA用. */
+	public static final String URL_BASE_SPA = "/${spring.profiles.active}/${application.version:v0.1}/spa";
+
+	/** URLのベース部分：MPA用. */
+	public static final String URL_BASE_MPA = "/${spring.profiles.active}/${application.version:v0.1}/mpa";
 
 }
